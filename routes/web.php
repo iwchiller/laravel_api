91075aa77ce68bbdfcab4d88f4417f6c3318a8bc
IW Chiller <iw_chiller@mail.ru>
2026-07-04 17:11:55 +0300

    Clear console

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,7 @@
 
 use App\Models\Sale;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SaleController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\IncomeController;
-use App\Http\Controllers\StockController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::resource('sales', SaleController::class);
-Route::resource('orders', OrderController::class);
-Route::resource('incomes', IncomeController::class);
-Route::resource('stocks', StockController::class);
-
-Route::get('/api/orders', function () {
-    return \App\Models\Order::where('id', 34)->get();
 });
