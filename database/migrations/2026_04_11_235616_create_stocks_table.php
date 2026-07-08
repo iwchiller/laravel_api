00ac5f50c7cedf05555d5aa8af8245bad6476d6e
IW Chiller <iw_chiller@mail.ru>
2026-07-08 03:14:46 +0300

    Calculated field

--- database/migrations/2026_04_11_235616_create_stocks_table.php
+++ database/migrations/2026_04_11_235616_create_stocks_table.php
@@ -34,7 +34,7 @@ return new class extends Migration
             $table->string('price', 40)->nullable();
             $table->string('discount', 40)->nullable();
             $table->string('hash_sha1', 40)
-                ->virtualAs('sha1(CONCAT_WS(date, last_change_date, supplier_article,
+                ->storedAs('sha1(CONCAT_WS(date, last_change_date, supplier_article,
                     tech_size, barcode, quantity, is_supply, is_realization, quantity_full,
 	                warehouse_name, in_way_to_client, in_way_from_client, nm_id, subject,
                     category, brand, sc_code, price, discount))')
